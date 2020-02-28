3016723f678508dbbc22bbfd45cc467132f16fa7
Deijah Price <deijahwilson1@gmail.com>
2020-02-28 04:56:31 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -38,8 +38,9 @@ end
 def total_gross(source)
    total_gross_of_earnings = 0
     director_index = 0
+    
  	  while director_index < source.size do
-	    total_gross_of_earnings += gross_for_direc
+	    total_gross_of_earnings += gross_for_director
 	    source[director_index] )
       director_index += 1
  end
